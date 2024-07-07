aa750c8e63d30db5434326befb51231be7ec47f1
Muthuraj-Vairamuthu <muthuraj22307@iiitd.ac.in>
2024-07-07 19:57:43 +0530

    myenv

--- src/portfolio.js
+++ src/portfolio.js
@@ -1,6 +1,6 @@
 const header = {
   // all the properties are optional - can be left empty or deleted
-  homepage: 'https://Muthuraj-Vairamuthu.github.io',
+  homepage: 'https://Muthuraj-Vairamuthu.github.io/Portfolio-New',
   title: 'MV.',
 }
 
